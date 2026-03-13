5f82eca71183ba60d911f81994ddb4d26a1384d4
JLW <jamie.williams@virtuallaunch.pro>
2026-03-12 17:38:37 -0700

    Updated assets, build, contracts, deployed

--- build.mjs
+++ build.mjs
@@ -5,13 +5,16 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const DIST = path.join(__dirname, 'dist');
-const SITE = path.join(__dirname, 'site');
-const ASSETS = path.join(__dirname, 'assets');
-const PARTIALS = path.join(__dirname, 'partials');
-const STYLES = path.join(__dirname, 'styles');
+const DIST = path.resolve(__dirname, 'dist');
+const SITE = path.resolve(__dirname, 'site');
+const ASSETS = path.resolve(__dirname, 'assets');
+const PARTIALS = path.resolve(__dirname, 'partials');
+const STYLES = path.resolve(__dirname, 'styles');
 
 async function build() {
+  console.log('BUILD __dirname =', __dirname);
+  console.log('BUILD DIST =', DIST);
+
   await rm(DIST, { force: true, recursive: true });
   await mkdir(DIST, { recursive: true });
 
